4bbfa11cf908e6c3fc0a909debd65c7dc0fb154d
Om Baviskar <omrb2002@gmail.com>
2026-05-15 11:24:39 +0530

    Fixed CORS completely

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/{*splat}', cors(corsOptions));
 
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
